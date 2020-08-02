2ee2697e8ab1f9bd7eb3d90d15238a14eb7c429c
Elvinas Paskauskas <ElvinasPaskauskas@gmail.com>
2020-08-02 16:48:57 +0100

    cleaning up react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*Heaader*/}
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
+          alt="insta logo"
+        />
+        <h1> HEYO</h1>
+      </div>
+      {/*Post */}
+      {/*Post */}
     </div>
   );
 }
