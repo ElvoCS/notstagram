ddbe22bf8a6a59038ecdbf3ffc79154bc28b17d5
Elvinas Paskauskas <ElvinasPaskauskas@gmail.com>
2020-08-04 12:48:18 +0100

    setting up fb db + passing posts into db

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Post from "./Post";
 import "./App.css";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "Elvo",
+      caption: "Shes working",
+      imageUrl:
+        "https://i.guim.co.uk/img/media/6088d89032f8673c3473567a91157080840a7bb8/413_955_2808_1685/master/2808.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=412cc526a799b2d3fff991129cb8f030",
+    },
+    {
+      username: "Jessica",
+      caption: "Look at my mountain",
+      imageUrl: "https://i.imgur.com/eQbWtC9.jpg",
+    },
+  ]);
+
   return (
     <div className="App">
       {/*Heaader*/}
@@ -12,10 +26,16 @@ function App() {
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
           alt="insta logo"
         />
-        <h1> HEYO</h1>
       </div>
-      {/*Post */}
-      {/*Post */}
+      <h1> HEYO</h1>
+
+      {posts.map((post) => (
+        <Post
+          username={post.username}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyCv_c2xMw_nglq2jAJT0nfBfv0PhAVGrnI",
+  authDomain: "notstagram-4d20e.firebaseapp.com",
+  databaseURL: "https://notstagram-4d20e.firebaseio.com",
+  projectId: "notstagram-4d20e",
+  storageBucket: "notstagram-4d20e.appspot.com",
+  messagingSenderId: "295475244909",
+  appId: "1:295475244909:web:200e0fe93cb30f62d13464",
+  measurementId: "G-JC8ZQD54TG",
+});
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export { db, auth, storage };
